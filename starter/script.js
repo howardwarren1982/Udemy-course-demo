54081dda30996ff6ac1cfe9f92028ccdc5d169dc
howardwarren1982 <howardwarren89@gmail.com>
2021-05-11 13:20:23 -0400

    Initial

---
+++ starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const howard = new Person('how', 1991);
+
+// console.log(howard);
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+//car object
+//
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl('Jess', 1996);
+console.log(jessica);
+jessica.calcAge();
